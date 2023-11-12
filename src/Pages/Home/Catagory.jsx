44ec1bb678d148e287932a8562435431374818cf
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-11-12 16:32:19 +0600

    Iteam added

---
+++ src/Pages/Home/Catagory.jsx
@@ -0,0 +1,53 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination } from "swiper/modules";
+
+import img1 from "../../assets/home/slide1.jpg";
+import img2 from "../../assets/home/slide2.jpg";
+import img3 from "../../assets/home/slide3.jpg";
+import img4 from "../../assets/home/slide4.jpg";
+import img5 from "../../assets/home/slide5.jpg";
+
+const Catagory = () => {
+  return (
+    <div>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={img1} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-28">Salad</h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img2} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-28">Pizza </h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img3} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-28">Soup</h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img4} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-28">Salad</h2>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img5} alt="" />
+          <h2 className="text-4xl uppercase text-center -mt-16">Salad</h2>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Catagory;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import Banner from "./Banner";
+import Catagory from "./Catagory";
 
 
 const Home = () => {
     return (
         <div>
-           
         <Banner></Banner>
+        <Catagory></Catagory>
         </div>
     );
 };
