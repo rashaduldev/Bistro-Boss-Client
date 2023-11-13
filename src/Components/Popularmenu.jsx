8ecb0043ba1eea7791cbe1865aad79dc7a97513c
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-11-13 09:56:55 +0600

    Create Popularmenu.jsx

---
+++ src/Components/Popularmenu.jsx
@@ -0,0 +1,17 @@
+import SectionTitle from "./SectionTitle";
+
+
+const Popularmenu = () => {
+    return (
+        <section>
+            <SectionTitle
+            heading={"From our menu"}
+            subHeading={"Popular Item"}
+            >
+
+            </SectionTitle>
+        </section>
+    );
+};
+
+export default Popularmenu;
