a1294f5f71dd1e2e14b955253119537488b50c59
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-11-15 13:30:26 +0600

    testmonials done

--- src/Pages/Home/Testimonials.jsx
+++ src/Pages/Home/Testimonials.jsx
@@ -1,38 +1,47 @@
 import SectionTitle from "../../Components/SectionTitle";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
+import '@smastrom/react-rating/style.css'
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/navigation";
 
-import { Navigation } from 'swiper/modules';
+import { Navigation } from "swiper/modules";
 import { useEffect, useState } from "react";
+import { Rating } from "@smastrom/react-rating";
 
 const Testimonials = () => {
-    const [reviews,setReviews]=useState([]);
-    useEffect(()=>{
-        fetch('reviews.json')
-      .then(res=>res.json())
-      .then(data=>setReviews(data))
-    },[])
-    return (
-        <section>
-            <SectionTitle subHeading={'---What Our Clients Say---'} heading={'TESTIMONIALS'}></SectionTitle>
+  const [reviews, setReviews] = useState([]);
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data));
+  }, []);
+  return (
+    <section>
+      <SectionTitle
+        subHeading={"---What Our Clients Say---"}
+        heading={"TESTIMONIALS"}
+      ></SectionTitle>
 
-            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-      <div className="m-10">
-      {
-           reviews.map((review) => <SwiperSlide key={review.id}>
-            <div>
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        <div className="">
+          {reviews.map((review) => (
+            <SwiperSlide key={review.id}>
+              <div className="m-24 flex flex-col items-center space-y-4">
+                <Rating style={{ maxWidth: 180 }}
+                 value={review.rating}
+                  readOnly 
+                  />
                 <p>{review.details}</p>
                 <h3 className="text-2xl text-orange-400">{review.name}</h3>
-            </div>
-           </SwiperSlide>) 
-        }
-      </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </div>
       </Swiper>
-        </section>
-    );
+    </section>
+  );
 };
 
 export default Testimonials;
