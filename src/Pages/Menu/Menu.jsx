7e1368f7704149181eed412e724becb656a4b003
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-11-15 14:55:05 +0600

    react-parallax added

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,4 +1,7 @@
 import { Helmet} from 'react-helmet-async';
+import Cover from '../../Shared/Cover';
+import menuimg from '../../assets/menu/banner3.jpg'
+import Popularmenu from '../../Components/Popularmenu';
 
 const Menu = () => {
     return (
@@ -6,6 +9,12 @@ const Menu = () => {
              <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
+      <Cover img={menuimg} title="Our Manu"></Cover>
+      <Popularmenu></Popularmenu>
+      <Cover img={menuimg} title="Our Manu"></Cover>
+      <Popularmenu></Popularmenu>
+      <Cover img={menuimg} title="Our Manu"></Cover>
+      <Popularmenu></Popularmenu>
         </div>
     );
 };
---
+++ src/Shared/Cover.jsx
@@ -0,0 +1,33 @@
+import { Parallax } from 'react-parallax';
+
+const Cover = ({img,title}) => {
+  return (
+    <div>
+           <Parallax
+        blur={{ min: -15, max: 15 }}
+        bgImage={img}
+        bgImageAlt="the menu"
+        strength={-200}
+    >
+       <div
+        className="hero h-[600px]"
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">{title}</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+          </div>
+        </div>
+      </div>
+    </Parallax>
+     
+    </div>
+  );
+};
+
+export default Cover;
