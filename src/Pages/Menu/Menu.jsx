190fc3ee14b401d2da0cc33a9488b1a5cf000d61
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-11-15 17:03:24 +0600

    Menus added

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -2,19 +2,31 @@ import { Helmet} from 'react-helmet-async';
 import Cover from '../../Shared/Cover';
 import menuimg from '../../assets/menu/banner3.jpg'
 import Popularmenu from '../../Components/Popularmenu';
+import useMenu from '../../Hooks/useMenu';
+import SectionTitle from '../../Components/SectionTitle';
+import MenuCatagory from './MenuCatagory';
 
 const Menu = () => {
+  const [menu]=useMenu();
+  console.log(menu);
+  const dessert=menu.filter(item=>item.category==='dessert');
+  const soup=menu.filter(item=>item.category==='soup');
+  const pizza=menu.filter(item=>item.category==='pizza');
+  const offered=menu.filter(item=>item.category==='offered');
     return (
         <div>
              <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
       <Cover img={menuimg} title="Our Manu"></Cover>
-      <Popularmenu></Popularmenu>
-      <Cover img={menuimg} title="Our Manu"></Cover>
-      <Popularmenu></Popularmenu>
-      <Cover img={menuimg} title="Our Manu"></Cover>
-      <Popularmenu></Popularmenu>
+      {/* main cover */}
+      <SectionTitle subHeading={'---Dont miss---'} heading={'TODAY S OFFER'}></SectionTitle>
+      {/* offered */}
+      <MenuCatagory items={offered}></MenuCatagory>
+      
+      <MenuCatagory items={offered}></MenuCatagory>
+
+
         </div>
     );
 };
--- src/Pages/Menu/MenuCatagory.jsx
+++ src/Pages/Menu/MenuCatagory.jsx
@@ -1,11 +1,22 @@
+/* eslint-disable react/prop-types */
+import MenuIteam from "../../Shared/MenuIteam";
 
 
-const MenuCatagory = () => {
+const MenuCatagory = ({items}) => {
     return (
         <div>
-            
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 my-10">
+                {
+                    items.map(item=> <MenuIteam 
+                        key={item._id}
+                        item={item}
+                        ></MenuIteam>)
+                }
+                 
+            </div>
         </div>
     );
 };
+
 
 export default MenuCatagory;
