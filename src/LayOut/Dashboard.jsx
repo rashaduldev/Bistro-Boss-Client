5649f6d1d42d917193658692a40e87c94faa13a2
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-11-22 14:04:26 +0600

    Admin part added

--- src/LayOut/Dashboard.jsx
+++ src/LayOut/Dashboard.jsx
@@ -1,64 +1,121 @@
-import { FaArtstation, FaCalendar, FaCartShopping, FaHouseMedical, FaMarsAndVenus, FaMoneyBill, FaSearchengin } from "react-icons/fa6";
+import {
+  FaArtstation,
+  FaBook,
+  FaCalendar,
+  FaCartShopping,
+  FaHouseMedical,
+  FaList,
+  FaMarsAndVenus,
+  FaMoneyBill,
+  FaSearchengin,
+  FaUsers,
+  FaUtensils,
+} from "react-icons/fa6";
 import { NavLink, Outlet } from "react-router-dom";
 import useCart from "../Hooks/useCart";
 
-
 const Dashboard = () => {
-    const [cart]=useCart(); 
-    return (
-        <div className="flex">
-            <div className="w-64 min-h-screen bg-orange-500 hidden lg:block">
-                <ul className="menu">
-                        <li className="mb-2">
-                            <NavLink to={'/dashboard/userHome'}>
-                            <FaHouseMedical></FaHouseMedical>
-                            User Home</NavLink>
-                        </li>
-                        <li className="mb-2">
-                            <NavLink to={'/dashboard/cart'}>
-                            <FaCartShopping></FaCartShopping>
-                            My cart ({cart.length})</NavLink>
-                        </li>
-                        <li className="mb-2">
-                            <NavLink to={'/dashboard/reservation'}>
-                            <FaCalendar></FaCalendar>
-                            Reservation</NavLink>
-                        </li>
-                        <li className="mb-2">
-                            <NavLink to={'/dashboard/payment'}>
-                            <FaMoneyBill></FaMoneyBill>
-                            Payment History</NavLink>
-                        </li>
-                        <li className="mb-2">
-                            <NavLink to={'/dashboard/review'}>
-                            <FaArtstation></FaArtstation>
-                            Add Review</NavLink>
-                        </li>
-                        <li className="mb-2">
-                            <NavLink to={'/dashboard/booking'}>
-                           <FaMarsAndVenus></FaMarsAndVenus>
-                            My Booking</NavLink>
-                        </li>
-                        <div className="divider"></div>
-                        <li className="mb-2">
-                            <NavLink to={'/'}>
-                            <FaHouseMedical></FaHouseMedical>
-                            Home</NavLink>
-                        </li>
-                        <li className="mb-2">
-                            <NavLink to={'/order/salad'}>
-                            <FaSearchengin></FaSearchengin>
-                            Menu</NavLink>
-                        </li>
+  const [cart] = useCart();
+  const isAdmin = true;
+  return (
+    <div className="flex">
+      <div className="w-64 min-h-screen bg-orange-500 hidden lg:block">
+        <ul className="menu">
+          {isAdmin ? (
+            <>
+              <li className="mb-2">
+                <NavLink to={"/dashboard/adminHome"}>
+                  <FaHouseMedical></FaHouseMedical>
+                  Admin Home
+                </NavLink>
+              </li>
+              <li className="mb-2">
+                <NavLink to={"/dashboard/additems"}>
+                  <FaUtensils></FaUtensils>
+                 Add Items
+                </NavLink>
+              </li>
+              <li className="mb-2">
+                <NavLink to={"/dashboard/reservation"}>
+                  <FaList></FaList>
+                  Manage Items
+                </NavLink>
+              </li>
+              <li className="mb-2">
+                <NavLink to={"/dashboard/booking"}>
+                  <FaBook></FaBook>
+                  Manage Booking
+                </NavLink>
+              </li>
+              <li className="mb-2">
+                <NavLink to={"/dashboard/users"}>
+                  <FaUsers></FaUsers>
+                  All Users
+                </NavLink>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className="mb-2">
+                <NavLink to={"/dashboard/userHome"}>
+                  <FaHouseMedical></FaHouseMedical>
+                  User Home
+                </NavLink>
+              </li>
+              <li className="mb-2">
+                <NavLink to={"/dashboard/cart"}>
+                  <FaCartShopping></FaCartShopping>
+                  My cart ({cart.length})
+                </NavLink>
+              </li>
+              <li className="mb-2">
+                <NavLink to={"/dashboard/reservation"}>
+                  <FaCalendar></FaCalendar>
+                  Reservation
+                </NavLink>
+              </li>
+              <li className="mb-2">
+                <NavLink to={"/dashboard/payment"}>
+                  <FaMoneyBill></FaMoneyBill>
+                  Payment History
+                </NavLink>
+              </li>
+              <li className="mb-2">
+                <NavLink to={"/dashboard/review"}>
+                  <FaArtstation></FaArtstation>
+                  Add Review
+                </NavLink>
+              </li>
+              <li className="mb-2">
+                <NavLink to={"/dashboard/booking"}>
+                  <FaMarsAndVenus></FaMarsAndVenus>
+                  My Booking
+                </NavLink>
+              </li>
+            </>
+          )}
 
-
-                </ul>
-            </div>
-            <div className="flex-1 ">
-                <Outlet></Outlet>
-            </div>
-        </div>
-    );
+          <div className="divider"></div>
+          {/* Shared navlinks */}
+          <li className="mb-2">
+            <NavLink to={"/"}>
+              <FaHouseMedical></FaHouseMedical>
+              Home
+            </NavLink>
+          </li>
+          <li className="mb-2">
+            <NavLink to={"/order/salad"}>
+              <FaSearchengin></FaSearchengin>
+              Menu
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div className="flex-1 ">
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -0,0 +1,12 @@
+
+
+const AllUsers = () => {
+    return (
+        <div>
+            <div>All Users</div>    
+            <div>Totat Users</div>    
+        </div>
+    );
+};
+
+export default AllUsers;
