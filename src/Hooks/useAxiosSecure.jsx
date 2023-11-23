9ae0b0e7bebed36974b637bfba34d8d676411607
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-11-23 10:05:22 +0600

    Token logout

--- src/Hooks/useAxiosSecure.jsx
+++ src/Hooks/useAxiosSecure.jsx
@@ -1,9 +1,13 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from './useAuth';
 
 const axiosSecure=axios.create({
     baseURL:'http://localhost:3000'
 })
 const useAxiosSecure = () => {
+    const {logOut}=useAuth();
+    const navigate=useNavigate();
     // interceptors request
     axiosSecure.interceptors.request.use(function(config){
         const token=localStorage.getItem('access-token');
@@ -17,9 +21,13 @@ const useAxiosSecure = () => {
     // Add a response interceptor
     axiosSecure.interceptors.response.use(function (response) {
     return response;
-  }, function (error) {
+  }, async (error)=> {
     const status=error.response.status;
     console.log('Status error in the interceptor',status);
+    if (status=== 401 || status=== 403) {
+        await logOut();
+        navigate('/login');
+    }
     return Promise.reject(error);
   });
     return axiosSecure;
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -43,7 +43,8 @@ const AuthProvider = ({children}) => {
         return()=>{
             return unsubscribe();
         }
-    })
+    },[axiosPublic])
+    
     //Log in with Google
     const googleSignin=()=>{
         setLoading(true);
